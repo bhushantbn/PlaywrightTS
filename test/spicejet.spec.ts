46c48df6d97f0934a6bd37bf785024762d2d0648
mitchelstarc101 <mitchelstarc101@gmail.com>
2024-03-28 23:25:14 +0530

    updated testcases

--- test/spicejet.spec.ts
+++ test/spicejet.spec.ts
@@ -4,9 +4,9 @@ import { chromium,webkit,firefox } from '@playwright/test'
 test('move Element',async()=>{
     const browser:Browser=await chromium.launch({headless:false,channel:'chrome'})
     const page:Page=await browser.newPage()
-    page.goto("https://www.spicejet.com")
-    page.getByText('Add-ons').first().hover()
-    page.getByText('Taxi').first().click()
+    await page.goto("https://www.spicejet.com")
+    await page.getByText('Add-ons').first().hover()
+    await page.getByText('Taxi').first().click()
     await page.waitForTimeout(5000)
     await page.close()
 })

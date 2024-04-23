1faa78fc052ca229a05d8858d2d175608923b0d4
mitchelstarc101 <mitchelstarc101@gmail.com>
2024-04-23 23:21:47 +0530

    added randomPopup.spec.ts

--- test/randomPopup.spec.ts
+++ test/randomPopup.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("Handle random Popup", async ({ page }) => {
   await page.addLocatorHandler(
-    page.getByText("Subscribe to CommitQuality"),
+    page.getByText("Random Popup"),
     async () => {
-      await page.getByTestId("close-popup").click();
+      await page.getByRole("button",{name:'Close'}).click();
     }
   );
   await page.goto("https://commitquality.com/practice-random-popup");
   await new Promise((resolve) => setTimeout(resolve, 6000));
   await page.locator('[data-testid="accordion-1"]').click();
-  //await page.close()
+  await page.close()
 });

1553a3dbdeba4a32cb5a3d21e455c55c0bf9a0a2
bhushantbn <bhushantbn@gmail.com>
2024-05-18 14:32:22 +0530

    added context menu testcases

---
+++ test/contextMenu.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, Dialog } from "@playwright/test";
+import { assert } from "console";
+
+test.describe("Context Menu", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.lambdatest.com/selenium-playground");
+    await page.getByText("Context Menu").click();
+  });
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+  test("Verify Context menu dialog", async ({ page }) => {
+    page.on("dialog", async (dialog) => {
+      await page.waitForTimeout(3000);
+      expect(dialog.type()).toContain("alert");
+      expect(dialog.message()).toContain("You selected a context menu");
+      await dialog.accept();
+    });
+    await page.locator("#hot-spot").click({ button: "right" });
+  });
+});
